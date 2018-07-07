5619b1132b8ab5737458dbd4e8df4789a9712daa
George Liu <eva2000@centminmod.com>
2018-07-08 05:07:57 +1000

    WPT slack channel message format result urls

--- gitools.sh
+++ gitools.sh
@@ -600,7 +600,8 @@ wpt_run() {
                 message_color='#bb4a12'
               fi
             fi
-            slacksend "Webpagetest.org Test: $WPT_LOCATION\n$WPT_URL\n$WPT_USER_RESULTURL\n$WPT_USER_RESULTXMLURL\n$WPT_LIGHTHOUSE_URL\n$WPT_HISTORY_URL\n$send_message" "$DT - $WPT_LOCATION $WPT_SPEED" wpt $message_color
+            # slacksend "Webpagetest.org Test: $WPT_LOCATION\n$WPT_URL\n<$WPT_USER_RESULTURL|WPT Result Page>\n<$WPT_USER_RESULTXMLURL|WPT XML Result>\n<$WPT_LIGHTHOUSE_URL|Lighthouse Result Page>\n<$WPT_HISTORY_URL|WPT History Log>\n$send_message" "$DT - $WPT_LOCATION $WPT_SPEED" wpt $message_color
+            slacksend "Webpagetest.org Test: $WPT_LOCATION\n$WPT_URL\n<$WPT_USER_RESULTURL|WPT Result Page>\t<$WPT_USER_RESULTXMLURL|WPT XML Result>\n<$WPT_HISTORY_URL|WPT History Log>\t<$WPT_LIGHTHOUSE_URL|Lighthouse Result Page>\n$send_message" "$DT - $WPT_LOCATION $WPT_SPEED" wpt $message_color
           fi
         fi
         echo "----"
